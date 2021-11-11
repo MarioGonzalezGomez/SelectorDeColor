53c06b58e8979ce3029e36becfa272d9a1ef0824
MarioGonzalezGomez <ggperpi@gmail.com>
2021-11-11 12:42:45 +0100

    funcional con tamaño responsive

--- src/main/java/com/example/selectordecolores/Vista.java
+++ src/main/java/com/example/selectordecolores/Vista.java
@@ -1,11 +1,6 @@
 package com.example.selectordecolores;
 
-import javafx.beans.binding.NumberBinding;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.*;
@@ -29,9 +24,9 @@ public class Vista extends VBox {
     Slider sliderG = new Slider(0, 255, 0);
     Slider sliderB = new Slider(0, 255, 0);
 
-    NumberBinding valorR;
-    NumberBinding valorG;
-    NumberBinding valorB;
+    SimpleIntegerProperty valorR;
+    SimpleIntegerProperty valorG;
+    SimpleIntegerProperty valorB;
 
 
     public Vista() {
@@ -39,19 +34,28 @@ public class Vista extends VBox {
     }
 
     public void inciarElementos() {
+
         pantalla.setPrefSize(400, 100);
 
         sliderR.setShowTickMarks(true);
         sliderG.setShowTickMarks(true);
         sliderB.setShowTickMarks(true);
 
-        valorR = sliderR.valueProperty().divide(255);
-        valorG = sliderG.valueProperty().divide(255);
-        valorB = sliderB.valueProperty().divide(255);
+        valorR = new SimpleIntegerProperty();
+        valorG = new SimpleIntegerProperty();
+        valorB = new SimpleIntegerProperty();
+
+        valorR.bind(sliderR.valueProperty());
+        valorG.bind(sliderG.valueProperty());
+        valorB.bind(sliderB.valueProperty());
 
         labelValueR.textProperty().bind(valorR.asString());
         labelValueG.textProperty().bind(valorG.asString());
         labelValueB.textProperty().bind(valorB.asString());
+
+        labelValueR.setMinWidth(30);
+        labelValueG.setMinWidth(30);
+        labelValueB.setMinWidth(30);
 
         hboxR.getChildren().add(labelR);
         hboxR.getChildren().add(sliderR);
@@ -74,14 +78,22 @@ public class Vista extends VBox {
         labelG.setText("Green: ");
         labelB.setText("Blue: ");
 
-        // pantalla.setBackground(new Background(new BackgroundFill(Color.color(valorR.intValue(), valorG.intValue(), valorB.intValue()), null, null));
+        labelR.setMinWidth(40);
+        labelG.setMinWidth(40);
+        labelB.setMinWidth(40);
 
-        //pantalla.eightProperty()
-      //  pantalla.widthProperty()
+
+        sliderR.prefWidthProperty().bind(this.widthProperty().subtract(labelR.getWidth()).subtract(labelValueR.getWidth()));
+        sliderG.prefWidthProperty().bind(this.widthProperty().subtract(labelG.getWidth()).subtract(labelValueG.getWidth()));
+        sliderB.prefWidthProperty().bind(this.widthProperty().subtract(labelB.getWidth()).subtract(labelValueB.getWidth()));
+
+        pantalla.prefWidthProperty().bind(this.widthProperty());
+        pantalla.prefHeightProperty().bind(this.heightProperty().subtract(hboxR.getHeight()).multiply(3));
+
 
         valorR.addListener(e -> {
             BackgroundFill fill = new BackgroundFill(
-                    Color.color(valorR.doubleValue(), valorG.doubleValue(), valorB.doubleValue()),
+                    Color.rgb(valorR.intValue(), valorG.intValue(), valorB.intValue()),
                     null,
                     null);
             pantalla.setBackground(new Background(fill));
@@ -89,7 +101,7 @@ public class Vista extends VBox {
 
         valorG.addListener(e -> {
             BackgroundFill fill = new BackgroundFill(
-                    Color.color(valorR.doubleValue(), valorG.doubleValue(), valorB.doubleValue()),
+                    Color.rgb(valorR.intValue(), valorG.intValue(), valorB.intValue()),
                     null,
                     null);
             pantalla.setBackground(new Background(fill));
@@ -97,35 +109,12 @@ public class Vista extends VBox {
 
         valorB.addListener(e -> {
             BackgroundFill fill = new BackgroundFill(
-                    Color.color(valorR.doubleValue(), valorG.doubleValue(), valorB.doubleValue()),
+                    Color.rgb(valorR.intValue(), valorG.intValue(), valorB.intValue()),
                     null,
                     null);
             pantalla.setBackground(new Background(fill));
         });
-
-        // valores.add(valorR);
-        // valores.add(valorG);
-        // valores.add(valorB);
-
-        // pantalla.backgroundProperty().bind(valorR.asString("Valor del slider: %.d"));
-        // pantalla.backgroundProperty().bind(valorG.asString("Valor del slider: %.d"));
-        // pantalla.backgroundProperty().bind(valorB.asString("Valor del slider: %.d"));
-
-        //pantalla.backgroundProperty().bind(valores);
-
-
-        sliderR.valueProperty().addListener((observable, oldValue, newValue) -> {
-            // pantalla.setBackground(new Background(new BackgroundFill(Color.color(valorR.doubleValue(), valorG.doubleValue(), valorB.doubleValue())), CornerRadii.EMPTY, Insets.EMPTY);
-            //new Color(valorR.intValue(), valorG.intValue(), valorB.intValue())))
-        });
-
-        //valorR.addListener(new ChangeListener<Number>() {
-        //     @Override
-        //         pantalla.setText(valorR);
-        //    }
-        // });
     }
-
 
     protected void onHelloButtonClick() {
 
